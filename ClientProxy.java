bea66a983b516233263f9f8ae4edf0bcfb1c4aff
davidPies <2014119@edtools.psd401.net>
2016-05-20 09:05:49 -0700

    Update ClientProxy.java

--- ClientProxy.java
+++ ClientProxy.java
@@ -12,6 +12,7 @@ public class ClientProxy extends CommonProxy {
 
     public void init(FMLInitializationEvent e) {
     super.init(e);
+    
     }
 
     public void postInit(FMLPostInitializationEvent e) {
