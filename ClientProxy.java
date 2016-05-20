37f516c89d384ceb5ddcfdefa5d732b203c7155b
davidPies <2014119@edtools.psd401.net>
2016-05-20 09:09:03 -0700

    Update ClientProxy.java

--- ClientProxy.java
+++ ClientProxy.java
@@ -1,5 +1,7 @@
-package com.bedrockminer.tutorial;
+package dwsmod;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -8,11 +10,12 @@ public class ClientProxy extends CommonProxy {
 
     public void preInit(FMLPreInitializationEvent e) {
     super.preInit(e);
+    
+    Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(dwsmod.item1, 0, new ModelResourceLocation(dwsmod.modid + ":" + dwsmod.item1.getUnlocalizedName().substring(5), "inventory"));
     }
 
     public void init(FMLInitializationEvent e) {
     super.init(e);
-    
     }
 
     public void postInit(FMLPostInitializationEvent e) {
