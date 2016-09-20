a0f8ac52b5c2bd8e1eb7c899f55eb16d888425e5
davidPies <2014119@edtools.psd401.net>
2016-09-20 11:48:06 -0700

    Update item1.java

--- dwsmod/item1.java
+++ dwsmod/item1.java
@@ -1,11 +1,18 @@
+//reference: http://bedrockminer.jimdo.com/modding-tutorials/basic-modding-1-8/custom-tools-swords/
 package dwsmod;
 
 import net.minecraft.item.ItemSword;
+import net.minecraft.item.Item;
 
-public class ItemModSword extends ItemSword {
-}
+public class item1 extends ItemSword {
+    public static Item item1;
+    public static ToolMaterial SW1 = EnumHelper.addToolMaterial("SW1", 3, 1000, 15.0F, 4.0F, 30);
 public ItemModSword(ToolMaterial material) {
     super(material);
+      this.GameRegistry.registerItem(item1 = new item1(SW1), "item1");
       this.setUnlocalizedName("item1");
-      this. ();
+      this.setCreativeTab(CreativeTabs.MISC);
+      
+    }
+    
 }
