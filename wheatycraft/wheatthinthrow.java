193a4f5cf479ae3291a96ca6ca2f69a67b1681fb
davidPies <2014119@edtools.psd401.net>
2016-09-26 11:32:33 -0700

    Create wheatthinthrow.java

---
+++ wheatycraft/wheatthinthrow.java
@@ -0,0 +1,11 @@
+package wheatycraft;
+
+import net.minecraft.item.ItemSnowball;
+
+public class wheatthinthrow extends ItemSnowball{
+  public wheatthin(){
+    this.maxStackSize = 32;
+    this.setCreativeTab(creativeTabs.COMBAT);
+    this.setResourceLocation("wheatycraft:wheatthinthrow");
+  }
+}
