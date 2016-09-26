33eb10a7dea52ac590692bb2bcbb5036bf71ba5c
davidPies <2014119@edtools.psd401.net>
2016-09-26 11:22:26 -0700

    Create server.java

---
+++ wheatycraft/server.java
@@ -0,0 +1,24 @@
+package wheatycraft;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+public class server extends proxy {
+
+    public void preInit(FMLPreInitializationEvent e) {
+    super.preInit(e);
+    
+    }
+
+    public void init(FMLInitializationEvent e) {
+    super.init(e);
+    
+    }
+
+    public void postInit(FMLPostInitializationEvent e) {
+    super.postInit(e);
+    }
+}
